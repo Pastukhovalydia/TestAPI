e526b665604c0927f1c212684837357b0ac8f5bb
Pastukhovalydia <pastuhovna@gmail.com>
2024-01-08 14:07:46 +0300

    testAPI

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,47 @@
 package ru.netology.rest;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static io.restassured.filter.log.LogDetail.ALL;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
+
+    private RequestSpecification requestSpec;
+
+    @BeforeEach
+    void setUp() {
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri("http://localhost")
+                .setBasePath("/api/v1")
+                .setPort(9999)
+                .setAccept(ContentType.JSON)
+                .setContentType(ContentType.JSON)
+                .log(ALL)
+                .build();
+    }
+
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("", hasSize(greaterThan(0)))
+                .body("[0].id", notNullValue())
+                .body("[0].name", not(emptyOrNullString()))
+                .body("[0].number", matchesPattern("^•• \\d{4}$"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+                .body("[0].currency", not(emptyOrNullString()));
     }
 }
